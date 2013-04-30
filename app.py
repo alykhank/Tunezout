551733af1e3e5d7fb2cf105aeb184106bfb4a5d2
Alykhan Kanji <github@alykhan.com>
2013-04-30 18:08:01 -0400

    Implement filtering by genre and viewing all.

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 genres = ('Hip Hop', 'Electronic', 'R&B')
-songs = [\
+songlist = [\
 		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':'2013', 'genre':'Hip Hop' },\
-		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':'2012', 'genre':'Hip Hop' },\
+		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':'2013', 'genre':'Hip Hop' },\
 		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':'2013', 'genre':'Electronic' }\
 		]
 
 @app.route('/')
 def index():
-	return render_template('index.html', genres=genres, genre=genres[0], songs=songs)
+	genreFilter = request.args.get('Genre')
+	if genreFilter and 0 <= int(genreFilter) and int(genreFilter) < len(genres):
+		genre = genres[int(genreFilter)]
+		songs = copyf('genre', [genre])
+	else:
+		genre = 'All'
+		songs = songlist
+	return render_template('index.html', genres=genres, genre=genre, songs=songs)
 
 @app.route('/submit')
 def submit():
@@ -20,8 +27,11 @@ def submit():
 	artist = request.args.get('Artist')
 	year = request.args.get('Year')
 	genre = request.args.get('Genre')
-	songs.append({ 'rank':str(len(songs) + 1), 'title':title, 'artist':artist, 'year':year, 'genre':genre })
+	songlist.append({ 'rank':str(len(songlist) + 1), 'title':title, 'artist':artist, 'year':year, 'genre':genre })
 	return redirect(url_for('index'))
+
+def copyf(key, valuelist):
+	return [dictio for dictio in songlist if dictio[key] in valuelist]
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
