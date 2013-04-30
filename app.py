b182699d44a672499d7344a9fba733dd6b30f913
Alykhan Kanji <github@alykhan.com>
2013-04-29 23:05:58 -0400

    Add basic template.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-	return 'Tunezout'
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
