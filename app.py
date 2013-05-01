43969c3e32ab6af133fe6f106743d199f98e635d
Alykhan Kanji <github@alykhan.com>
2013-04-30 20:23:52 -0400

    Add ratings UI.

--- app.py
+++ app.py
@@ -5,42 +5,42 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 genres = ('Country', 'Dance/Electronic', 'Hip-Hop/Rap', 'Jazz', 'R&B', 'Rock', 'Other')
 songlist = [\
-		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
-		{ 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
-		{ 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 }\
+		{ 'id':1, 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':2, 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':3, 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':4, 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':5, 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':6, 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':7, 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':8, 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':9, 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':10, 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':11, 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':12, 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':13, 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':14, 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':15, 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':16, 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':17, 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':18, 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':19, 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':20, 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':21, 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':22, 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':23, 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':24, 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':25, 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':26, 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':27, 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':28, 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':29, 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':30, 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':31, 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':32, 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':33, 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'id':34, 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':35, 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'id':36, 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 }\
 		]
 
 @app.route('/')
