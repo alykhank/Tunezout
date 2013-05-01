19bed0d0f579f9877d29949599fcaadf7fa92595
Alykhan Kanji <github@alykhan.com>
2013-04-30 20:16:02 -0400

    Add more genres, improve safety of inputs.

--- app.py
+++ app.py
@@ -3,18 +3,51 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-genres = ('Hip Hop', 'Electronic', 'R&B')
+genres = ('Country', 'Dance/Electronic', 'Hip-Hop/Rap', 'Jazz', 'R&B', 'Rock', 'Other')
 songlist = [\
-		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':'2013', 'genre':'Hip Hop' },\
-		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':'2013', 'genre':'Hip Hop' },\
-		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':'2013', 'genre':'Electronic' }\
+		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'1', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'2', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'3', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'4', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'5', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'6', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'7', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'8', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'9', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 },\
+		{ 'rank':'10', 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'11', 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':12, 'down':4 },\
+		{ 'rank':'12', 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':12, 'down':4 }\
 		]
 
 @app.route('/')
 def index():
-	genreFilter = request.args.get('Genre')
-	if genreFilter and 0 <= int(genreFilter) and int(genreFilter) < len(genres):
-		genre = genres[int(genreFilter)]
+	genreFilter = request.args.get('Genre', type=int)
+	if genreFilter and 0 < genreFilter and genreFilter < len(genres):
+		genre = genres[genreFilter - 1]
 		songs = copyf('genre', [genre])
 	else:
 		genre = 'All'
@@ -23,11 +56,11 @@ def index():
 
 @app.route('/submit')
 def submit():
-	title = request.args.get('Song Title')
-	artist = request.args.get('Artist')
-	year = request.args.get('Year')
-	genre = request.args.get('Genre')
-	if (title and artist and year and genre):
+	title = request.args.get('Song Title', type=str)
+	artist = request.args.get('Artist', type=str)
+	year = request.args.get('Year', type=int)
+	genre = request.args.get('Genre', type=str)
+	if (title and artist and year and genre and genre in genres):
 		songlist.append({ 'rank':str(len(songlist) + 1), 'title':title, 'artist':artist, 'year':year, 'genre':genre })
 	return redirect(url_for('index'))
 
