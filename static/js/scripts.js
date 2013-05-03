8f96c7fb7a4b3c15dabc7d5c9b3f14082baf5e3d
Alykhan Kanji <github@alykhan.com>
2013-05-03 00:45:01 -0400

    Use global  instead of passing value.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,13 @@
-function rate(songId, rating, genre) {
+function rate(songId, rating) {
 	$.get($SCRIPT_ROOT + '/rate', {
 		ID: songId,
 		Rate: rating
 	});
-	updateSongs(genre);
+	updateSongs();
 	return false;
 }
-function updateSongs(genre) {
-	$.getJSON($SCRIPT_ROOT + '/songs', { 'Genre':genre }, function(data) {
+function updateSongs() {
+	$.getJSON($SCRIPT_ROOT + '/songs', { 'Genre':$GENRE }, function(data) {
 		$('tbody').html('');
 		$('table caption').text(data.genre);
 		jQuery.each(data.songs, function(index, song) {
@@ -20,10 +20,10 @@ function updateSongs(genre) {
 			+ '<td> <span class="badge badge-inverse">' + song.score + '</span>'
 			+ '&nbsp;'
 			+ '<div class="btn-group">'
-				+ '<a class="btn" onclick="rate(' + song.id + ', 2, ' + genre + ')">'
+				+ '<a class="btn" onclick="rate(' + song.id + ', 2, ' + $GENRE + ')">'
 					+ '<span class="text-success">' + song.up + '</span> <i class="icon-thumbs-up"></i>'
 				+ '</a>'
-				+ '<a class="btn" onclick="rate(' + song.id + ', 1, ' + genre + ')">'
+				+ '<a class="btn" onclick="rate(' + song.id + ', 1, ' + $GENRE + ')">'
 					+ '<span class="text-error">' + song.down + '</span> <i class="icon-thumbs-down"></i>'
 				+ '</a>'
 			+ '</div>'
