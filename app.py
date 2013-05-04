51fc121768f45e4bbc424a5c8cbd1ace88f1fe03
Alykhan Kanji <github@alykhan.com>
2013-05-04 13:41:54 -0400

    Allow blank song titles and artists.

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def submit():
 	year = request.args.get('year', 0, type=int)
 	genre = request.args.get('genre', 'Other', type=str)
 	genreMatch = Genre.query.filter(Genre.name == genre).first()
-	if title and artist and genreMatch:
+	if genreMatch:
 		song = Song(title, artist, year, genreMatch, 0, 0, 0)
 		db.session.add(song)
 		db.session.commit()
