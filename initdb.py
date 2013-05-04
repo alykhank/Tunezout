b1b7491ee3442236d49fa1e521f4df3e08bf1b22
Alykhan Kanji <github@alykhan.com>
2013-05-04 11:28:15 -0400

    Rewrite models with relationship from songs to genre.

--- initdb.py
+++ initdb.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 from models import db, Song, Genre
 
-genres = ('Country', 'Dance/Electronic', 'Hip-Hop/Rap', 'House', 'Jazz', 'R&B', 'Rock', 'Other')
+genres = (Genre('Country'), Genre('Dance/Electronic'), Genre('Hip-Hop/Rap'), Genre('House'), Genre('Jazz'), Genre('R&B'), Genre('Rock'), Genre('Other'))
+
+for genre in genres:
+	db.session.add(genre)
 
 songlist = [\
-	{ 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':0, 'down':0, 'score':0 },\
-	{ 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':'Hip-Hop/Rap', 'up':0, 'down':0, 'score':0 },\
-	{ 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':'Dance/Electronic', 'up':0, 'down':0, 'score':0 }\
+	{ 'title':'The Motto', 'artist':'Drake', 'year':2013, 'genre':genres[2], 'up':0, 'down':0, 'score':0 },\
+	{ 'title':'Started from the Bottom', 'artist':'Drake', 'year':2013, 'genre':genres[2], 'up':0, 'down':0, 'score':0 },\
+	{ 'title':'Thrift Shop', 'artist':'Macklemore', 'year':2013, 'genre':genres[1], 'up':0, 'down':0, 'score':0 }\
 	]
 
 for item in songlist:
 	song = Song(item['title'], item['artist'], item['year'], item['genre'], item['up'], item['down'], item['score'])
 	db.session.add(song)
-for item in genres:
-	genre = Genre(item)
-	db.session.add(genre)
 db.session.commit()
