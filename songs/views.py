1697ec3a6e029e400bef4313557594832bf3969c
Alykhan Kanji <github@alykhan.com>
2013-09-26 23:05:33 -0400

    Enable songs app, create generic ListView index

--- songs/views.py
+++ songs/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
+from songs.models import Genre, Song
+
+class IndexView(generic.ListView):
+	template_name = 'songs/index.html'
+
+	def get_queryset(self):
+		return Song.objects.order_by('-score')
