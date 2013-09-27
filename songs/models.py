56f7978a890750f0292515c00b2dd70ef2aa796c
Alykhan Kanji <github@alykhan.com>
2013-09-26 22:25:06 -0400

    Create models for Genre and Song

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return self.name
+
+class Song(models.Model):
+	title = models.CharField(max_length=100)
+	artist = models.CharField(max_length=100)
+	year = models.DateField()
+	genre = models.ForeignKey(Genre)
+	up = models.PositiveIntegerField(default=0)
+	down = models.PositiveIntegerField(default=0)
+	score = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.title
