54b68b2aa18c07f65bb27c464e68c86f22cf2576
Alykhan Kanji <github@alykhan.com>
2013-09-27 00:14:40 -0400

    Add year view

--- songs/urls.py
+++ songs/urls.py
@@ -5,4 +5,5 @@ from songs import views
 urlpatterns = patterns('',
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^year/(?P<year>\d+)/$', views.YearView.as_view(), name='year'),
 )
--- songs/views.py
+++ songs/views.py
@@ -12,3 +12,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
 	model = Song
 	template_name = 'songs/detail.html'
+
+class YearView(generic.ListView):
+	template_name = 'songs/index.html'
+
+	def get_queryset(self):
+		return Song.objects.filter(year__year=self.kwargs['year']).order_by('-score')
