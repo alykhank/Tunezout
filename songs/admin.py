7e371ddcfcc7fdcdea190f76209be66c6b8831cb
Alykhan Kanji <github@alykhan.com>
2013-09-26 22:43:09 -0400

    Add contextual data to admin site

--- songs/admin.py
+++ songs/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from songs.models import Genre, Song
 
-admin.site.register(Genre)
-admin.site.register(Song)
+class SongInline(admin.TabularInline):
+	model = Song
+
+class GenreAdmin(admin.ModelAdmin):
+	inlines = [SongInline]
+
+class SongAdmin(admin.ModelAdmin):
+	list_display = ('title', 'artist', 'year', 'genre', 'up', 'down', 'score')
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Song, SongAdmin)
