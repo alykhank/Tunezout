e7ad096d89d223911a459564dc14105a23756296
Alykhan Kanji <github@alykhan.com>
2013-09-27 01:52:57 -0400

    Enable new song submission

--- songs/urls.py
+++ songs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 	url(r'^year/(?P<year>\d+)/$', views.YearView.as_view(), name='year'),
+	url(r'^submit/$', views.submit, name='submit'),
 )
--- songs/views.py
+++ songs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.views import generic
 
 from songs.models import Genre, Song
@@ -9,6 +11,13 @@ class IndexView(generic.ListView):
 	def get_queryset(self):
 		return Song.objects.order_by('-score')
 
+	def get_context_data(self, **kwargs):
+		# Call the base implementation first to get a context
+		context = super(IndexView, self).get_context_data(**kwargs)
+		# Add in a QuerySet of all the genres
+		context['genre_list'] = Genre.objects.all().order_by('-name')
+		return context
+
 class DetailView(generic.DetailView):
 	model = Song
 	template_name = 'songs/detail.html'
@@ -18,3 +27,38 @@ class YearView(generic.ListView):
 
 	def get_queryset(self):
 		return Song.objects.filter(year__year=self.kwargs['year']).order_by('-score')
+
+	def get_context_data(self, **kwargs):
+		# Call the base implementation first to get a context
+		context = super(YearView, self).get_context_data(**kwargs)
+		# Add in a QuerySet of all the genres
+		context['genre_list'] = Genre.objects.all().order_by('-name')
+		return context
+
+def submit(request):
+	genre_list = Genre.objects.all().order_by('-name')
+	try:
+		title = request.POST['title']
+		artist = request.POST['artist']
+		year = request.POST['year'] + '-01-01'
+		genre = get_object_or_404(Genre, pk=request.POST['genre'])
+	except (KeyError, Genre.DoesNotExist):
+		# Redisplay the song submission form.
+		return render(request, 'songs/index.html', {
+			'genre_list': genre_list,
+			'error_message': "Your submission was invalid.",
+		})
+	else:
+		try:
+			s = Song(title=title, artist=artist, year=year, genre=genre)
+			s.save()
+		except (ValueError):
+			# Redisplay the song submission form.
+			return render(request, 'songs/index.html', {
+				'genre_list': genre_list,
+				'error_message': "Your submission was invalid.",
+			})
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('songs:index'))
