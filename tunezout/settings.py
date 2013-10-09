acb1a8cfc542f9cff7957e55ddf1ed6b53be65cc
Alykhan Kanji <github@alykhan.com>
2013-10-09 02:37:51 -0400

    Add SendGrid email settings

--- tunezout/settings.py
+++ tunezout/settings.py
@@ -185,3 +185,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
