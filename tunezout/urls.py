54192d901c9ef085264829b084a3763e5ffa78fa
Alykhan Kanji <github@alykhan.com>
2013-10-08 23:32:38 -0400

    Add basic login/logout functionality

--- tunezout/urls.py
+++ tunezout/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tunezout.views.home', name='home'),
     url(r'^', include('songs.urls', namespace='songs')),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
